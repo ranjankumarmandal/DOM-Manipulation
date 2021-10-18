0122f9f895ae5e4e8409387198e5ce1b26059c00
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-10-18 19:22:02 +0530

    creating elements with vanilla javascript

---
+++ 04_create_elements.js
@@ -0,0 +1,29 @@
+// create element
+const li = document.createElement('li');
+
+// add class
+li.className = 'collection-item';
+
+// add id
+li.id = 'new-item';
+
+// add attribute
+li.setAttribute('title', 'New Item');
+
+// create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// create new link element
+const link = document.createElement('a');
+// add class
+link.className = 'delete-item secondary-content';
+// add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+// append link into li
+li.appendChild(link);
+
+// append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
